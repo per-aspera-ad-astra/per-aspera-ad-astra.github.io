d034edf44e1f49f88839bc590ce20de5b3686626
alexey <alex.vasilenko13@gmail.com>
2018-01-31 16:16:31 +0200

    some changes

--- js/main.js
+++ js/main.js
@@ -15,4 +15,18 @@ $(function(){
 
   enableVideos();
 
+
+  // Svg placeholder
+  $('.phone-line').find('input').on('focus blur', function(e){
+
+    var $icon = $(this).closest('.phone-line').find('.icon-phone');
+
+    if($(this).is(':focus')){
+      $icon.removeClass('show').addClass('hide');
+    } else if(!e.target.value){
+      $icon.removeClass('hide').addClass('show');
+    }
+
+  })
+
 });
